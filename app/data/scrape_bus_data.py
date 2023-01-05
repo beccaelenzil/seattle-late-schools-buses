d69e6012ce66c80ceab304ef694270b4bcacd219
Becca Elenzil <beccaelenzil@gmail.com>
2023-01-05 09:50:46 -0800

    update date regex

--- app/data/scrape_bus_data.py
+++ app/data/scrape_bus_data.py
@@ -3,21 +3,19 @@ import urllib.request as urllib
 import re
 
 
-
- 
-def scrape_late_bus_data(url): 
+def scrape_late_bus_data(url):
     # web scraping
     late_busses = url
     page = urllib.urlopen(late_busses)
     soup = BeautifulSoup(page, "html.parser")
     return soup
 
+
 def parse_late_bus_data(soup):
     paragraphs = soup.find_all('p')
     bus_pattern = re.compile("Route")
     date_pattern = re.compile(", 20")
 
-    
     bus_list = []
 
     time = "am"
@@ -28,17 +26,23 @@ def parse_late_bus_data(soup):
             time = "mid"
         elif "PM Delays" == paragraph:
             time = "pm"
-        
+
         if bus_pattern.search(paragraph):
             bus_list.append(paragraph+" "+time)
         elif date_pattern.search(paragraph):
             date = paragraph
 
-    date_pattern = re.compile("(January|February|March|April|May|June|July|August|September|October|November|December) (\d+), (\d\d\d\d)")
-    date_match = date_pattern.search(date)
+    print("*****", date, "*********")
 
-    bus_pattern = re.compile("Route (\d+) – ([A-Za-z ]+) – (\d+) ([A-Za-z]+).*(am|mid|pm)")
-    
+    date_pattern = re.compile(
+        "(January|February|March|April|May|June|July|August|September|October|November|December) (\d+)\S+, (\d\d\d\d)")
+
+    date_match = date_pattern.search(date)
+    print("*****", date_match, "*********")
+
+    bus_pattern = re.compile(
+        "Route (\d+) – ([A-Za-z ]+) – (\d+) ([A-Za-z]+).*(am|mid|pm)")
+
     new_late_buses = []
     for bus in bus_list:
         bus_match = bus_pattern.search(bus)
@@ -53,7 +57,5 @@ def parse_late_bus_data(soup):
             bus_dictionary["units"] = bus_match[4]
             bus_dictionary["time"] = bus_match[5]
             new_late_buses.append(bus_dictionary)
-    
+
     return new_late_buses
-
-
